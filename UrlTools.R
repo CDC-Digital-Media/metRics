5240c448840b4c16e0e0d0f1f7b5a416571dbfcb
Marcie Davis <marciejdavis@gmail.com>
2017-03-08 12:09:46 -0500

    Added a few more common default pages

--- UrlTools.R
+++ UrlTools.R
@@ -3,6 +3,10 @@ CleanUrl <- function(url) {
   #remove common default pages
   url <- gsub("/default.html$", "", url)
   url <- gsub("/index.html$", "", url)
+  url <- gsub("/default.htm$", "", url)
+  url <- gsub("/index.htm$", "", url)
+  url <- gsub("/default.asp$", "", url)
+  url <- gsub("/default.aspx$", "", url)
   
   url <- gsub("http://www.cdc", "https://www.cdc", url) # switch from http to https for cdc.gov sites
   
