03999c05fa7b539b629a5772c996dee029ae6c89
Marcie Davis <marciejdavis@gmail.com>
2017-03-30 08:06:13 -0400

    Further refinements to CDC top topics list

--- cdc_topics.R
+++ cdc_topics.R
@@ -7,7 +7,7 @@ library(tcltk2)
 
 topics <- fromJSON("https://tools.cdc.gov/api/v2/resources/tags?max=0")
 results <- topics$results
-eng_topics <- results[results$language == "English" & results$type == "Topic",]
+eng_topics <- subset(results, results$language == "English" & results$type == "Topic")
 eng_topics$rank <- -1 #add a column for rank so I can populate it from Google Trends
 
 PopulateNext = function() {
@@ -40,7 +40,6 @@ PopulateNext = function() {
 }
 
 PopulateNext()
-PopulateTrending()
 
 PopulateTrending = function()
 {
@@ -53,3 +52,83 @@ PopulateTrending = function()
   trending_topics <<- trending_topics[-which(trending_topics$name %in% ignore),]
   #trending_topics <<- trending_topics[-which(trending_topics$name == "Other" | trending_topics$name == "Funding" | trending_topics$name == "Travel health - delete"),]
 }
+
+rows = nrow(trending_topics)
+#chunk it by 5
+
+s5 <- seq(1, rows, 5)
+trending_topics$rank15 <- -1
+row.names(trending_topics) <- trending_topics$name
+
+for(i in s5) {
+  last <- ifelse(i +4 < rows, i + 4, rows)
+  terms <- trending_topics[i:last,]$name
+  trends <- gtrends(keyword = terms, cat = "45") #Health
+  overtime <- trends$interest_over_time
+
+  for(j in 1:5) {
+    termj_overtime <- overtime[overtime$keyword == terms[j] & overtime$gprop == "web",]
+    trending_topics[terms[j],"rank15"] <- max(termj_overtime$hits)
+  }
+}
+trending_topics2 <- subset(trending_topics, rank15 != 0)
+
+trending_topics2$rank15b <- -1
+
+#Cull again
+for(i in s5) {
+  last <- ifelse(i +4 < rows, i + 4, rows)
+  terms <- trending_topics2[i:last,]$name
+  trends <- gtrends(keyword = terms, cat = "45") #Health
+  overtime <- trends$interest_over_time
+  
+  for(j in 1:5) {
+    termj_overtime <- overtime[overtime$keyword == terms[j] & overtime$gprop == "web",]
+    trending_topics2[terms[j],"rank15b"] <- max(termj_overtime$hits)
+  }
+}
+trending_topics3 <- subset(trending_topics2, rank15b != 0)
+
+trending_topics3$rank15c <- -1
+
+#Cull again
+for(i in s5) {
+  last <- ifelse(i +4 < rows, i + 4, rows)
+  terms <- trending_topics3[i:last,]$name
+  trends <- gtrends(keyword = terms, cat = "45") #Health
+  overtime <- trends$interest_over_time
+  
+  for(j in 1:5) {
+    termj_overtime <- overtime[overtime$keyword == terms[j] & overtime$gprop == "web",]
+    trending_topics3[terms[j],"rank15c"] <- max(termj_overtime$hits)
+  }
+}
+trending_topics4 <- subset(trending_topics3, rank15c != 0)
+
+
+#now slice into different groups
+rows4 <- nrow(trending_topics4)
+s1_26 <- seq(1, rows4, 25) #1, 26, 51, etc
+trending_topics4$rank16 <- -1
+
+for (i in s1_26) {
+  s1_6 <- seq(1, rows4, 5) #1, 6, 11, etc
+  last <- ifelse(i + 4 < rows4, i + 4, rows4)
+  for (j in i:last) {
+    lastj <- ifelse(j +20 < rows4, j + 20, rows4)
+    indexes <- seq(j, lastj, 5)
+    terms <- trending_topics4[indexes, "name"]
+    trends <- gtrends(keyword = terms, category = "45") #Health
+    overtime <- trends$interest_over_time
+    for (k in terms){
+      termk_overtime <- overtime[overtime$keyword == k & overtime$gprop == "web",]
+      trending_topics4[k, "rank16"] <- max(termk_overtime$hits)
+    }
+    
+  }
+}
+trending_topics5 <- subset(trending_topics4, rank16 != 0)
+
+#third and final grouping
+rows5 <- nrow(trending_topics5)
+
